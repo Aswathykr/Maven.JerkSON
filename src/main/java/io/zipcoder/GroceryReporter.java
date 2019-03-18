57368cb9a10141827138fb45bbb31d79071aada1
Aswathy Kanakarajan <aswathykanakarajan@zipcoders-MacBook-Pro-7.local>
2019-03-18 15:41:10 -0400

    implemented and tested

--- src/main/java/io/zipcoder/GroceryReporter.java
+++ src/main/java/io/zipcoder/GroceryReporter.java
@@ -1,16 +1,123 @@
 package io.zipcoder;
 
 import io.zipcoder.utils.FileReader;
+import io.zipcoder.utils.Item;
+
+import java.util.*;
+import java.util.Map.Entry;
 
 public class GroceryReporter {
     private final String originalFileText;
+    private List<Item> itemList;
+    private List<ItemRecord> recordList;
 
     public GroceryReporter(String jerksonFileName) {
         this.originalFileText = FileReader.readFile(jerksonFileName);
+        ItemParser parser = new ItemParser();
+        itemList = parser.parseItemList(originalFileText);
+        recordList = new ArrayList<>();
+        for(Item item : itemList){
+            ItemRecord itemRecord = null;
+            if(item != null) {
+                itemRecord = new ItemRecord(item.getName());
+                if(!recordList.contains(itemRecord)){
+                    itemRecord.format(itemList, item.getName());
+                    recordList.add(itemRecord);
+                }
+            }
+
+        }
     }
 
     @Override
     public String toString() {
-        return null;
+        String result = "";
+
+        for(ItemRecord record : recordList){
+            result += record;
+        }
+        return result;
+    }
+
+    private class ItemRecord{
+        private String name;
+
+
+        private Integer itemFrequency;
+        private Map<Double, Integer> priceMap;
+
+        public Integer getItemFrequency() {
+            return itemFrequency;
+        }
+       public ItemRecord(String name) {
+            this.name = name;
+            itemFrequency = 0;
+            priceMap = new HashMap<>();
+        }
+
+        public String getName() {
+            return name;
+        }
+
+
+        public void format(List<Item> list, String name){
+            for(Item item : list) {
+                if(item.getName().equals(name)){
+                    itemFrequency ++;
+                    Integer frequency = priceMap.getOrDefault(item.getPrice(), null);
+                    if(frequency == null){
+                        priceMap.put(item.getPrice(), 1);
+                    }
+                    else {
+                        frequency = frequency.intValue() + 1;
+                        priceMap.put(item.getPrice(), frequency);
+                    }
+                }
+            }
+        }
+
+        @Override
+        public String toString() {
+            String result = "";
+            String priceSeparator = "-------------\t\t-------------\n";
+            if(!name.equals("Error")) {
+                String heading = "name: "+getFormatedName()+"\t\tseen: "+ itemFrequency + " times\n";
+                result = heading;
+                String separator = "=============\t\t=============\n";
+                result += separator;
+                int index = 0;
+                Set<Double> prices = priceMap.keySet();
+                for (Double key : prices) {
+                    String price = "Price: 	 " + key + "\t\tseen: "
+                            + priceMap.get(key) + " times\n" ;
+                    result += price;
+                    if(index < prices.size()-1 || prices.size() == 1)
+                        result += priceSeparator;
+                    index++;
+                }
+                result += "\n";
+            }
+            else{
+
+                String heading = name+"\t\t\t\tseen: "+ itemFrequency + " times\n";
+                result = heading;
+            }
+            return result;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (!(o instanceof ItemRecord)) return false;
+            ItemRecord that = (ItemRecord) o;
+            return Objects.equals(name, that.name);
+        }
+        public String getFormatedName(){
+            return String.format("%7s", Character.toUpperCase(name.charAt(0)) + name.substring(1));
+        }
+        @Override
+        public int hashCode() {
+            return Objects.hash(name);
+        }
     }
 }
--- src/main/java/io/zipcoder/ItemParser.java
+++ src/main/java/io/zipcoder/ItemParser.java
@@ -3,14 +3,53 @@ package io.zipcoder;
 import io.zipcoder.utils.Item;
 import io.zipcoder.utils.ItemParseException;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class ItemParser {
-    public List<Item> parseItemList(String valueToParse) {
-        return null;
+    private final String delimiterPattern = "[:@;!\\^\\%\\*]";
+    private final String toMatchPattern =
+            "naMe" + delimiterPattern+"(\\w*)" + delimiterPattern +
+            "price" + delimiterPattern+ "(\\d*.?\\d*)" + delimiterPattern +
+            "type" + delimiterPattern + "(\\w+)" + delimiterPattern +
+            "expiration" + delimiterPattern + "(\\d{1,2}/\\d{1,2}/\\d{2,4})##";
+
+    public List<Item> parseItemList(String valueToParse)  {
+
+        List<Item> list = new ArrayList<>();
+        Pattern pattern = Pattern.compile(toMatchPattern, Pattern.CASE_INSENSITIVE);
+        Matcher matcher = pattern.matcher(valueToParse);
+        while(matcher.find()) {
+            try {
+                list.add(parseSingleItem(valueToParse.substring(matcher.start(), matcher.end())));
+            }catch(ItemParseException ex){
+                list.add(new Item("Error",0.0,null, null));
+            }
+        }
+        return list;
     }
 
     public Item parseSingleItem(String singleItem) throws ItemParseException {
-        return null;
+
+
+        Pattern pattern = Pattern.compile(toMatchPattern, Pattern.CASE_INSENSITIVE);
+        Matcher matcher = pattern.matcher(singleItem);
+        matcher.find();
+        Item item = null;
+        try {
+
+            String name = matcher.group(1).toLowerCase();
+            if(name.isEmpty())
+                throw new ItemParseException();
+            name = name.replace('0','o');
+            item = new Item(name, Double.parseDouble(matcher.group(2)),
+                    matcher.group(3).toLowerCase(),
+                    matcher.group(4).toLowerCase());
+        }catch (Exception ex){
+            throw new ItemParseException();
+        }
+        return item;
     }
 }
